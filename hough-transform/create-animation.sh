2f49424ee8ba73928f9b8d82942d9bccff628366
Thales Sehn Körting <tkorting@gmail.com>
2017-11-13 12:03:22 -0200

    Update create-animation.sh

--- hough-transform/create-animation.sh
+++ hough-transform/create-animation.sh
@@ -1,3 +1,7 @@
 # this script allows to create a .mov file after crating png steps
+# run octave with main file
+octave main.m
+
+# produce animation with output
 rm -f hough-animation.mov
 ffmpeg -f image2 -pattern_type glob -i "animation/animation-*.png" -r 25 -b 50000000 -filter:v "setpts=0.5*PTS" -an hough-animation.mov
