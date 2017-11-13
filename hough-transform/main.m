8cfe086e1844480d795e03d24b7993d63d7d19df
Thales Sehn Körting <tkorting@gmail.com>
2017-11-13 12:02:06 -0200

    Update and rename ht.m to main.m
    making code generic for potential users

---
+++ hough-transform/main.m
@@ -0,0 +1,18 @@
+clear all;
+
+# defining input parameters for the algorithm
+input_image_name = 'input.png';
+original_image = imread(input_image_name);
+edge_image = edge(original_image, "Canny");
+theta_step = 1;
+resampling = 1;
+n_maximuns = 30;
+display_debug = true;
+make_animation = true;
+[matrix_A, rhos_thetas_rankings] = hough_transform(edge_image, theta_step, resampling, n_maximuns, display_debug, make_animation);
+
+# saving data for future uses, if needed
+save matrix_A.mat matrix_A;
+save rhos_thetas_rankings.mat rhos_thetas_rankings;
+save edge_image.mat edge_image;
+save original_image.mat original_image;
