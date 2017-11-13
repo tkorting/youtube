5b6dc3ace49d4ff32d0a03442b0f2dbd8aad067e
Thales Sehn Körting <tkorting@gmail.com>
2017-11-13 12:04:33 -0200

    Update show_lines.m

--- hough-transform/show_lines.m
+++ hough-transform/show_lines.m
@@ -1,3 +1,7 @@
+# this file can be used in case you produced all animation from octave, and want to display
+# the main lines detected by the hough transform
+# it will load matrices saved in main.m
+
 load matrix_A.mat;
 load rhos_thetas_rankings.mat;
 load edge_image.mat;
@@ -40,40 +44,3 @@ for line_id = 1:max_rhos
   y2 = ceil((rho - x2 * cos(theta * pi / 180)) / sin(theta * pi / 180));
   drawEdge(y1, x1, y2, x2, 'color', colorstr(line_id), 'linewidth', 2);
 endfor
-
-#  x1 = 1;
-#  y1 = 1;
-#  x2 = 1;
-#  y2 = 1;
-#  flag_can_stop_1 = false;
-#  flag_can_stop_2 = false;
-#  for x = 1:n_cols
-#    if (flag_can_stop_1 && flag_can_stop_2)
-#      continue;
-#    endif
-#    if (x1 == 1 && y1 == 1)
-#      y = ceil((rho - x * cos(theta * pi / 180)) / sin(theta * pi / 180));
-#      if (y > 0 && y <= n_rows)
-#        if (edge_image(y, x) > 0)
-#          x1 = x;
-#          y1 = y;
-#          flag_can_stop_1 = true;
-#        endif
-#      endif
-#    endif
-#    if (x2 == 1 && y2 == 1)
-#      x_end = n_cols - x;
-#      y_end = ceil((rho - x_end * cos(theta * pi / 180)) / sin(theta * pi / 180));
-#      if (y_end > 0 && y_end <= n_rows)
-#        if (edge_image(y_end, x_end) > 0)
-#          x2 = x_end;
-#          y2 = y_end;
-#          flag_can_stop_2 = true;
-#        endif
-#      endif
-#    endif
-#  endfor
-#
-#  drawEdge(y1, x1, y2, x2, 'color', colorstr(line_id), 'linewidth', 2);
-#endfor 
-#print('lines.png', '-dpng');
