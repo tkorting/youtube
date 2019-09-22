193c44010ad66d61fb3d5bff851c40520132babf
Thales Sehn Körting <tkorting@gmail.com>
2019-09-22 13:55:30 -0300

    Update create-movie.sh

--- maximum-likelihood-estimation/create-movie.sh
+++ maximum-likelihood-estimation/create-movie.sh
@@ -1,3 +1,10 @@
+# Subscribe to my channel, share and like my videos at
+# http://youtube.com/tkorting
+#
+# Feel free to use and share this code.
+#
+# Thales Sehn Körting
+
 # this script allows to create a .mov file after crating png steps
 # run octave with main file
 octave main.m
