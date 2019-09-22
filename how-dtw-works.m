53441779498857b32dadaf92dcdecf5f5008ced4
Thales Sehn Körting <tkorting@gmail.com>
2019-09-22 13:48:25 -0300

    Update how-dtw-works.m

--- how-dtw-works.m
+++ how-dtw-works.m
@@ -1,7 +1,16 @@
+# Subscribe to my channel, share and like my videos at
+# http://youtube.com/tkorting
+#
+# This source code is used to create a step-by-step
+# animation about image enhancement. Feel free to 
+# use and share this code.
+#
+# Thales Sehn Körting
+
 A = [1, 3, 4, 9, 8, 2, 1, 5, 7, 3];
 B = [1, 6, 2, 3, 0, 9, 4, 3, 6, 3];
-eixo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+dtw_axis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 figure(1), clf, hold on
-plot(eixo, A, 'r-', 'linewidth', 2.0)
-plot(eixo, B, 'b-', 'linewidth', 2.0), legend('time series A', 'time series B')
+plot(dtw_axis, A, 'r-', 'linewidth', 2.0)
+plot(dtw_axis, B, 'b-', 'linewidth', 2.0), legend('time series A', 'time series B')
 axis([0, 9])
