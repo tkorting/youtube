6c7c6dd164398f4be704a90600e33ecd951f6126
Thales Sehn Körting <tkorting@gmail.com>
2019-09-22 13:55:40 -0300

    Update main.m

--- maximum-likelihood-estimation/main.m
+++ maximum-likelihood-estimation/main.m
@@ -1,3 +1,10 @@
+# Subscribe to my channel, share and like my videos at
+# http://youtube.com/tkorting
+#
+# Feel free to use and share this code.
+#
+# Thales Sehn Körting
+
 clear all;
 
 # defining class 1 parameters
