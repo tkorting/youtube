5fe0b9d39c28966e774c20fb6cc1f0c990206918
Thales Sehn Körting <tkorting@gmail.com>
2019-09-22 13:50:06 -0300

    Update create_animation.sh

--- basic-ship-detection-in-rs/create_animation.sh
+++ basic-ship-detection-in-rs/create_animation.sh
@@ -1,3 +1,12 @@
+# Subscribe to my channel, share and like my videos at
+# http://youtube.com/tkorting
+#
+# This source code is used to create a step-by-step
+# animation about image enhancement. Feel free to 
+# use and share this code.
+#
+# Thales Sehn Körting
+
 # run ffmpeg to create animatino from steps
 ffmpeg -f image2 -pattern_type glob -i "animation/threshold_ndwi_step_*.png" -r 25 -b 50000000 -filter:v "setpts=0.5*PTS" -an threshold_ndwi_animation.flv
 ffmpeg -f image2 -pattern_type glob -i "animation/ships_in_rgb_step_*.png" -r 25 -b 50000000 -filter:v "setpts=0.5*PTS" -an ships_in_rgb_animation.flv
