9aceb38dc7c0c3cef65a6b022b9b5873e9070fee
Thales Sehn Körting <tkorting@gmail.com>
2019-09-22 13:50:58 -0300

    Update artificial_neuron.m

--- deep-learning-cnn/artificial_neuron.m
+++ deep-learning-cnn/artificial_neuron.m
@@ -1,3 +1,12 @@
+# Subscribe to my channel, share and like my videos at
+# http://youtube.com/tkorting
+#
+# This source code is used to create a step-by-step
+# animation about image enhancement. Feel free to 
+# use and share this code.
+#
+# Thales Sehn Körting
+
 # this function assumes that input_vector and weights_vector are of the same size, and 1D
 function an = artificial_neuron(input_vector, weights_vector, bias = 0)
 
