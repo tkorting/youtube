15a905e2edc4ccf2a67955911082da0ea61317c5
Thales Sehn Körting <tkorting@gmail.com>
2019-09-22 13:49:07 -0300

    Update main.py

--- basic-change-detection-in-rs/main.py
+++ basic-change-detection-in-rs/main.py
@@ -1,3 +1,12 @@
+# Subscribe to my channel, share and like my videos at
+# http://youtube.com/tkorting
+#
+# This source code is used to create a step-by-step
+# animation about image enhancement. Feel free to 
+# use and share this code.
+#
+# Thales Sehn Körting
+
 # importing libraries
 # needed to open GeoTIFF files
 from osgeo import gdal
