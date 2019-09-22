a79437b1454de5e5c2b2dc41d1c44ded32c01794
Thales Sehn Körting <tkorting@gmail.com>
2019-09-22 13:51:07 -0300

    Update convolution.m

--- deep-learning-cnn/convolution.m
+++ deep-learning-cnn/convolution.m
@@ -1,3 +1,12 @@
+# Subscribe to my channel, share and like my videos at
+# http://youtube.com/tkorting
+#
+# This source code is used to create a step-by-step
+# animation about image enhancement. Feel free to 
+# use and share this code.
+#
+# Thales Sehn Körting
+
 # this function assumes that input_matrix and weights_matrix are of the same size, and 2D
 function conv = convolution(input_matrix, weights_matrix)
 
